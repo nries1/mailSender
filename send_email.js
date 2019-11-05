b87b9199efbbaacb56c486f697680823902c972d
nries1 <nries1@gmail.com>
2019-11-05 11:05:18 -0500

    Update send_email.js

--- send_email.js
+++ send_email.js
@@ -1,47 +1,39 @@
-function doGet(e) {
-  //handle possible failures
-  //1) missing the package parameter
-  if (!e.parameter.package) return ContentService.createTextOutput(JSON.stringify({"msg": "mail send failed", "sent": false, "error": "Error: Missing required parameter: 'package'"}))
-  
-  //2) not enough email quota remaining
-  if (MailApp.getRemainingDailyQuota() <= 0) return ContentService.createTextOutput(JSON.stringify({"msg": "mail send failed", "sent": false, "error": "Error: The Mail Sender application has reached it send limits for the day. Please try again tomorrow" }))
-  var req;
-  
-  //3) package parameter is not proper JSON
-  try {
-    req = JSON.parse(e.parameter.package);
-  } catch(error) {
-    return ContentService.createTextOutput(JSON.stringify({"msg": "mail send failed", "sent": false, "error": "Error: Attempt to parse the paackage parameter rerturned the following error: "+error}))
+const sendEmail = (recipient, subject, body, options) => {
+  //handle errors
+  //1 recipient, subject, body parameters are not strings
+  if (![recipient, subject, body].every(arg => typeof arg === "string"))
+    throw new Error(
+      "The recipient, subject, and body parameters must be strings"
+    );
+  //2) the object parameter is not an object
+  if (
+    options !== undefined &&
+    Object.prototype.toString.call(options).match(/Object/g) === null
+  )
+    throw new Error("the options parameter must be an object");
+  const package = encodeURIComponent(
+    JSON.stringify({
+      recipient,
+      subject,
+      body,
+      options
+    })
+  );
+  //3) the body contains html but the options object does not contain an htmlBody property
+  if (
+    body.indexOf("<") !== -1 &&
+    body.indexOf("</") !== -1 &&
+    !options.hasOwnProperty("htmlBody")
+  ) {
+    throw new Error(
+      "Your email body appears to contain HTML. But you have not provided an htmlBody property to the options parameter. In order to send an email with html in the body, set the 'htmlBody' property in the options object equal to the html string that you want to send."
+    );
   }
-  
-  //4) the recipient, subject or body parameters are not type string, or the options param is not an object
-  if (typeof req.recipient !== 'string') return ContentService.createTextOutput(JSON.stringify({"msg": "mail send failed", "sent": false, "error": "Error: The recipient parameter must be of type 'string'"}));
-  if (typeof req.subject !== 'string') return ContentService.createTextOutput(JSON.stringify({"msg": "mail send failed", "sent": false, "error": "Error: The subject parameter must be of type 'string'"}));
-  if (typeof req.body !== 'string') return ContentService.createTextOutput(JSON.stringify({"msg": "mail send failed", "sent": false, "error": "Error: The body parameter must be of type 'string'"}));
-  if (!isObject(req.options)) return ContentService.createTextOutput(JSON.stringify({"msg": "mail send failed", "sent": false, "error": "Error: The options parameter must be of type 'object Object.'"}));
-  
-  //5) the body contains html but the options object does not contain an htmlBody property
-  if (req.body.indexOf("<") !== -1 &&
-      req.body.indexOf("</") !== -1 && 
-      !req.options.hasOwnProperty("htmlBody")) return ContentService.createTextOutput(JSON.stringify({"msg": "mail send failed", "sent": false, "error": "It looks like you are trying to pass html to the body to the mail sender. If that is the case, mail sender requires that the htmlBody property in the options object be set to the value of your html body."}));
-    try  {
-      MailApp.sendEmail(req.recipient, req.subject, req.body, req.options || {})
-      var out = {"msg": "mail sent", "sent": true}
-    }
-    catch(err) {
-      var out = {"msg": "mail send failed", "sent": false, "error": err}
-      MailApp.sendEmail('nries1@gmail.com', 'mail send failed', err);
-    }
-  finally {
-    return ContentService.createTextOutput(JSON.stringify(out));
-  }
-}
-
-function isObject(obj) {
-  return Object.prototype.toString.call(obj).match(/Object/g) !== null
-}
-
-function test() {
-  var package = {recipient: "nries@schools.nyc.gov", subject: "this is a subject", body: "<div>Body</div>", options: {htmlBody: "<div>Body</div>"}}
-  Logger.log(UrlFetchApp.fetch("https://script.google.com/macros/s/AKfycbxUOJGr0pqAiKBbDRqrn9H2mgr6Rtk3X0FR632SCLH40GgSBRE/exec?package="+encodeURIComponent(JSON.stringify(package))))
-}
+  const url = `https://tinyurl.com/emailfordevs?package=${package}`;
+  return new Promise((resolve, reject) => {
+    window
+      .fetch(url, { method: "GET" })
+      .then(res => resolve(res))
+      .catch(err => reject(err));
+  });
+};
